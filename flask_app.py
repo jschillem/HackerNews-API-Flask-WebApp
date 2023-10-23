772b38ed015d5420ffb397ec1f84ea246db9f845
Justin Schilleman <justin@schilleman.com>
2023-10-23 15:11:37 -0400

    created seeding script + inital flask app/db

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,30 @@
 from flask import Flask
-app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
 
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+
+
+class NewsItem(db.Model):
+    __tablename__ = 'news_items'
+
+    id = db.Column(db.Integer, primary_key=True)
+    by = db.Column(db.String)
+    descendants = db.Column(db.Integer)
+    score = db.Column(db.Integer)
+    text = db.Column(db.Text)
+    time = db.Column(db.Integer)
+    type = db.Column(db.String)
+    url = db.Column(db.String)
+    # Omitting the 'kids' field as this is not part of our project requirements
 
 @app.route("/")
 def hello():
-    return "<h1>Hello world!</h1>"
+    return "<h1>Hello yall!</h1>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ hackernews_api_grab.py
@@ -0,0 +1,45 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from flask_app import NewsItem, app
+import requests
+import time
+
+app.app_context().push()
+
+instance_folder = app.instance_path
+
+engine = create_engine(f'sqlite:////{instance_folder}/site.db')
+Session = sessionmaker(bind=engine)
+
+API_URL = "https://hacker-news.firebaseio.com/v0"
+topPosts = requests.get(f"{API_URL}/topstories.json").json()
+
+counter = 0
+start = time.process_time()
+with Session() as session:
+    try:
+        for postId in topPosts:
+            item = session.query(NewsItem).filter_by(id=postId).first()
+
+            # if the ID is not already in the database
+            if item is None:
+                post = requests.get(f"{API_URL}/item/{postId}.json").json()
+                if post['type'] == 'story':
+                    new_item = NewsItem(
+                        id=int(post['id']),
+                        by=post.get('by', None),
+                        descendants=post.get('descendants', None),
+                        score=int(post.get('score', None)),
+                        text=post.get('text', None),
+                        time=int(post.get('time', None)),
+                        type=post['type'],
+                        url=post.get('url', None)
+                    )
+                    session.add(new_item)
+                    counter += 1
+    finally:
+        session.commit()
+        session.close()
+
+elapsed = time.process_time() - start
+print(f'It took {elapsed} seconds to add {counter} news items.')
